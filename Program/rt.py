5f908476ba066de2acda853f62353914fc7fa5ca
Jake Whitaker <jake.whitaker@gmail.com>
2014-05-24 16:25:00 -0500

    cleaned up rt.py

--- Program/rt.py
+++ Program/rt.py
@@ -1,31 +1,37 @@
 import logging
 
-class routing_table(object):
+class RoutingTable(object):
     def __init__(self):
         self.rt = {}
 
-    def add_rt_entry(self, rt_entry):
+    def AddRTEntry(self, rt_entry):
         if rt_entry.get_name() not in rt.keys():
             self.rt[rt_entry.get_name()] = rt_entry
         else:
             print "ERROR: entry already in routing table, this should never happen"
 
-    def rem_rt_entry(self, name):
+    def RemRTEntry(self, name):
         rt_entry = self.find_rt_entry(name)
         if rt_entry != None:
             del self.rt[name]
 
-    def find_rt_entry(self, name):
+    def FindRTEntry(self, name):
         if name in rt.keys():
             return rt[name]
         else:
             return None
 
-class rt_entry(object):
+class RTEntry(object):
     def __init__(self, name, ringpos, timestamp):
         self.name = name
         self.ringpos = ringpos
         self.timestamp = timestamp
         
-    def get_name(self):
+    def GetName(self):
         return self.name
+
+    def GetRingPos(self):
+        return self.ringpos
+
+    def GetTimestamp(self):
+        return self.timestamp
