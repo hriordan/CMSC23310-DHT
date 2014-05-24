871ad88a07797c87e1a85a908186d37591e1531e
Jake Whitaker <jake.whitaker@gmail.com>
2014-05-24 17:39:17 -0500

    beginnings of message.py

---
+++ Program/message.py
@@ -0,0 +1,20 @@
+import json
+
+class Message(object):
+    def __init__(self):
+        self.type
+        self.dest
+        self.src
+
+class MessageGet(Message):
+    pass
+
+class MessageSet(Message):
+
+class MessageGetResponse(Message):
+
+class MessageSetResponse(Message):
+
+class MessageHello(Message):
+
+class MessageLog(Message):
