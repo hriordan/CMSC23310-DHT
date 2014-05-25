17e7eec18aff4cc3b58015d052be7c6f9289564d
Josh <jfeingold35@gmail.com>
2014-05-25 12:41:30 -0500

    Making progress on the parser.

--- Program/parse.py
+++ Program/parse.py
@@ -2,6 +2,19 @@
 
 import argparse
 
-parser = argparse.ArgumentParser(description='Parse arguments broker passes to node.')
-
-#parser.add_argument('--node-name', metavar = 'node',)
+parser = argparse.ArgumentParser()
+parser.add_argument("-n","--node-name", dest = "name",
+                    help = "Name of the node")
+parser.add_argument("-pep", "--pub-endpoint", dest = "pep",
+                    help = "public endpoint")
+parser.add_argument("-rep", "--router-endpoint", dest = "rep",
+                    help = "router endpoint")
+parser.add_argument("-pf", "--peers", dest = "peers",
+                    help = "A .csv containing the peer list")
+args = parser.parse_args()
+if args.name != None:
+    print args.name
+if args.pep != None:
+    print args.pep
+if args.rep != None:
+    print args.rep
