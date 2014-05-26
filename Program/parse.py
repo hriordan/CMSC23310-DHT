78075905c1b3dce835952090bf3527b3a2503a96
Josh <jfeingold35@gmail.com>
2014-05-26 11:02:41 -0500

    The parser is nearly done, from what I can tell. I'll try in a moment.

--- Program/parse.py
+++ Program/parse.py
@@ -1,7 +1,7 @@
 # parse.py
 
 import argparse
-
+"""
 parser = argparse.ArgumentParser()
 parser.add_argument("-n","--node-name", dest = "name",
                     help = "Name of the node")
@@ -10,7 +10,7 @@ parser.add_argument("-pep", "--pub-endpoint", dest = "pep",
 parser.add_argument("-rep", "--router-endpoint", dest = "rep",
                     help = "router endpoint")
 parser.add_argument("-pf", "--peers", dest = "peers",
-                    help = "A .csv containing the peer list")
+                    help = "A  containing the peer list")
 args = parser.parse_args()
 bundle = {}
 if args.name != None:
@@ -20,3 +20,22 @@ if args.pep != None:
 if args.rep != None:
     print args.rep
 if args.peers != None:
+"""
+class NodeArgs(object):
+    def __init__(self):
+        """ Parses the command line arguments. """
+        parser = argparse.ArgumentParser()
+        parser.add_argument("-n", "--node-name", dest = "name",
+                            help = "Name of the node")
+        parser.add_argument("-pep", "--pub-endpoint", dest = "pep",
+                            help = "public endpoint")
+        parser.add_argument("-rep", "--router-endpoint", dest = "rep",
+                            help = "router endpoint")
+        parser.add_argument("-pf", "--peers", dest = "peers",
+                            help = "The comma-separated peer list.")
+
+        args = parser.parse_args()
+        self.name = args.name
+        self.pep = args.pep
+        self.rep = args.rep
+        self.peers = args.peers.split(",")
